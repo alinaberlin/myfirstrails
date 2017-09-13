ea5afe267de13160d5bd5b238d4358e0a4ac67b8
Alina Ghetler <alina.ghetler@gmail.com>
2017-09-13 19:55:06 +0200

    routing and genered dynamic link in embeded Ruby (5.1 Routes)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 	def landing_page
-    	@products = Product.limit(3)
-    	#redirect_to products_url
+    	#@products = Product.limit(3)
+    	redirect_to products_url
   	end
 end
 
